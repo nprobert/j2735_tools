e00c5c1d96035b6f621f24e54b33f683281e5a43
Neal Probert <nprobert@probestar.net>
2024-09-20 11:46:40 -0400

    Don't need simpleaudio as it won't build anymore.

--- packages/packages.cmd
+++ packages/packages.cmd
@@ -4,6 +4,6 @@ python -m pip install --upgrade pip
 python -m pip install numpy pyside6 scapy pyyaml pykml tqdm virtualenv virtualenvwrapper
 
 python -m pip install --upgrade pip pyinstaller pycrate
-python -m pip install --upgrade ipython debugpy virtualenv virtualenvwrapper pyyaml tqdm simpleaudio
+python -m pip install --upgrade ipython debugpy virtualenv virtualenvwrapper pyyaml tqdm
 python -m pip install --upgrade pyside6 pyserial numpy numpydoc pandas plotly scipy
-python -m pip install --upgrade gps gpxpy maidenhead pykml pynmea pynmea2 pyyaml tqdm simpleaudio
+python -m pip install --upgrade gps gpxpy maidenhead pykml pynmea pynmea2 pyyaml tqdm
